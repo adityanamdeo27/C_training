af081e649826870f996228e3266f7e8687700e6e
adityanamdeo93-dotcom <adityanamdeo93@gmail.com>
2025-09-03 16:01:12 +0530

    Loop

---
+++ lec_6/Factorial.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n,fact=1;
+    printf("Enter a no. for Factorial:");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        fact = fact*i;
+    }
+    printf("Factorial is : %d",fact);
+    return 0;
+}
---
+++ lec_6/Prime.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int n,flag =1;
+    printf("Enter a no.:");
+    scanf("%d",&n);
+    for(int i=2;i<=n/2;i++){
+        if(n%i==0){
+            flag = 0;
+            break;
+        }
+
+    }
+    if(flag)
+        printf("%d is a prime number",n);
+    else
+        printf("%d is not a prime number",n);
+
+    return 0;
+}
