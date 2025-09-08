1eceedfee4ac299c4b6430b51ce3ffb4b33022f2
adityanamdeo27 <aditya.cs241025@global.org.in>
2025-09-08 14:03:10 +0530

    pattern

---
+++ lec_8/nested_loop.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+    for(int i = 0;i < 2;i++){
+        for(int j = 0;j < 2;j++){
+            printf("%d",j);
+        }
+    }
+}
---
+++ lec_8/space_tri.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main(){
+    int n;
+    printf("Enter a no. :");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        for(int j=n;j>i;j--){
+            printf(" ");
+        }
+        for(int k=0;k<=i;k++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
