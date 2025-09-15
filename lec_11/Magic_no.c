dec04c446993f423f1a8793957eb80b8616f6ba4
adityanamdeo27 <aditya.cs241025@global.org.in>
2025-09-15 13:09:44 +0530

    function

--- lec_11/Magic_no.c
+++ lec_11/Magic_no.c
@@ -9,18 +9,17 @@ int digit_sum(int n){
         sum = sum + lastdigit;
         n = n/10;
     }
-    int d = sum;
-    return sum;
-    //return digit_sum(d);
+   // return sum;
+    return digit_sum(sum);
 }
 void main(){
     int n,sum = 0;
     printf("Enter a no. :");
     scanf("%d",&n);
     sum = digit_sum(n);
-    while(sum>=10){
-        sum = digit_sum(sum);
-    }
+    // while(sum>=10){
+    //     sum = digit_sum(sum);
+    // }
     printf("last sum :%d\n",sum);
     if(sum==1)
         printf("It is a magic number");
